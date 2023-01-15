88eb66b4ed7b6093921de0dabe294132982a602d
Pavel Pavlov <pavelsvpavlov@gmail.com>
2023-01-15 23:56:42 +0200

    Update secret_door's_lock.py

--- python_basics_sep_2022/nested_loops/additional_nested_loops/secret_door's_lock.py
+++ python_basics_sep_2022/nested_loops/additional_nested_loops/secret_door's_lock.py
@@ -5,7 +5,7 @@ ones_upper_num = int(input())
 for idx_1 in range(1, hundreds_upper_num + 1):
     if idx_1 % 2 == 0:
         for idx_2 in range(2, tens_upper_num + 1):
-            for i in range(2, (idx_2 // 2) + 1):
+            for i in range(2, int(idx_2 / 2) + 1):
                 if idx_2 % i == 0:
                     break
             else:
