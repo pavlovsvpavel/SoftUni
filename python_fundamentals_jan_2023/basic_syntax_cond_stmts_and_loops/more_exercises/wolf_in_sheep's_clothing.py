498bbd3b6e4954f05f9d59294d06d9fddb745e5e
Pavel Pavlov <pavelsvpavlov@gmail.com>
2023-01-16 11:45:56 +0200

    Update wolf_in_sheep's_clothing.py

--- python_fundamentals_jan_2023/basic_syntax_cond_stmts_and_loops/more_exercises/wolf_in_sheep's_clothing.py
+++ python_fundamentals_jan_2023/basic_syntax_cond_stmts_and_loops/more_exercises/wolf_in_sheep's_clothing.py
@@ -1,6 +1,20 @@
+string = input()
 
+string_list = string.split(", ")
+counter = 0
+flag = False
 
+for i in range(len(string_list) - 1, - 1, - 1):
+    if string_list[len(string_list) - 1] == "wolf":
+        break
+    if string_list[i] == "wolf":
+        flag = True
+        break
+    if string_list[i] == "sheep":
+        counter += 1
 
-
-
+if flag:
+    print(f"Oi! Sheep number {counter}! You are about to be eaten by a wolf!")
+else:
+    print("Please go away and stop eating my sheep")
 
