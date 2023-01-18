d29f1f093ba40319b50e9693bd860e73f5c5104b
Pavel Pavlov <pavelsvpavlov@gmail.com>
2023-01-18 21:09:31 +0200

    code update

--- python_fundamentals_jan_2023/data_types_and_variables/lab/special_numbers.py
+++ python_fundamentals_jan_2023/data_types_and_variables/lab/special_numbers.py
@@ -1,11 +1,12 @@
 num = int(input())
 
 for i in range(1, num + 1):
-    first_digit = i // 10
-    second_digit = i % 10
-    sum_of_digits = first_digit + second_digit
+    sum_of_digits = 0
+    single_digit = i
+    while single_digit > 0:
+        sum_of_digits += single_digit % 10
+        single_digit = (single_digit // 10)
     if str(sum_of_digits) in ["5", "7", "11"]:
         print(f"{i} -> True")
     else:
         print(f"{i} -> False")
-
