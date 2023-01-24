cb6270ba755171dcda27c6dc9019afa29fb0eddb
Pavel Pavlov <pavelsvpavlov@gmail.com>
2023-01-24 17:02:44 +0200

    code optimization

--- python_fundamentals_jan_2023/lists_basics/exercises/football_cards.py
+++ python_fundamentals_jan_2023/lists_basics/exercises/football_cards.py
@@ -3,32 +3,30 @@ red_cards = input()
 number_of_players = 11
 team_a = []
 team_b = []
-card_check = []
-player_number = []
 less_players = False
+
 for player in range(1, number_of_players + 1):
-    team_a.append(str(player))
-    team_b.append(str(player))
+    team_a.append(player)
+    team_b.append(player)
 
 if "A" not in red_cards and "B" not in red_cards:
     print(f"Team A - {len(team_a)}; Team B - {len(team_b)}")
     exit()
 
-cards_list = red_cards.split(" ")
+cards_list = red_cards.split()
 
 # Checking every red card and remove player from team
-for card_check in cards_list:
-    player_number = card_check.split("-")
+for i in cards_list:
+    card_check = i.split("-")
+    team = card_check[0]
+    player_number = int(card_check[1])
 
-    if player_number[1] not in team_a or player_number[1] not in team_b:
-        continue
-
-    if "A" in card_check:
-        team_a.remove(player_number[1])
-    elif "B" in card_check:
-        team_b.remove(player_number[1])
-    else:
-        continue
+    if team == "A":
+        if player_number in team_a:
+            team_a.remove(player_number)
+    elif team == "B":
+        if player_number in team_b:
+            team_b.remove(player_number)
 
     if len(team_a) < 7 or len(team_b) < 7:
         less_players = True
