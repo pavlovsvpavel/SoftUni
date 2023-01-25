4102cf9228bd3f5a022ec0ba3e33406b6777a053
Pavel Pavlov <pavelsvpavlov@gmail.com>
2023-01-25 15:18:52 +0200

    Create faro_shuffle.py

---
+++ python_fundamentals_jan_2023/lists_basics/exercises/faro_shuffle.py
@@ -0,0 +1,17 @@
+cards_deck = input().split(" ")
+shuffles = int(input())
+
+half_deck_length = int(len(cards_deck) / 2)
+counter = 0
+
+for _ in range(shuffles):
+    counter = 0
+
+    for position in range(1, len(cards_deck) - 1):
+        if position % 2 != 0:
+            cards_deck.insert(position, cards_deck.pop(half_deck_length + counter))
+            counter += 1
+        else:
+            continue
+
+print(cards_deck)
