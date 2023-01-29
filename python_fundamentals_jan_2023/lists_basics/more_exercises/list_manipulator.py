41f928a5983bc8cb1224ed283417dc669118af21
Pavel Pavlov <pavelsvpavlov@gmail.com>
2023-01-29 13:24:49 +0200

    Update list_manipulator.py

--- python_fundamentals_jan_2023/lists_basics/more_exercises/list_manipulator.py
+++ python_fundamentals_jan_2023/lists_basics/more_exercises/list_manipulator.py
@@ -1,69 +1,112 @@
+from sys import maxsize
 my_list = [int(x) for x in input().split(" ")]
-
 
 while True:
     command = input().split(" ")
-    if "end" in command:
-        break
 
-    even_odd_list = []
+    if command[0] == "end":
+        break
 
     if command[0] == "exchange":
         idx_1 = int(command[1])
 
-        if idx_1 > len(my_list) - 1 or idx_1 < 0:
+        if idx_1 >= len(my_list) or idx_1 < 0:
             print("Invalid index")
-            continue
-
-        my_list = my_list[idx_1 + 1:] + my_list[:idx_1 + 1]
-
-    if command[0] == "max" or command[0] == "min":
-        idx_2 = command[1]
-        if idx_2 == "odd":
-            for i in my_list:
-                if i % 2 != 0:
-                    even_odd_list.append(i)
-        elif idx_2 == "even":
-            for i in my_list:
-                if i % 2 == 0:
-                    even_odd_list.append(i)
-
-        if not even_odd_list:
-            print("No matches")
-            continue
-
-        if command[0] == "max":
-            max_num = max(*even_odd_list)
-            print(my_list.index(max_num))
         else:
-            min_num = min(*even_odd_list)
-            print(my_list.index(min_num))
+            my_list = my_list[idx_1 + 1:] + my_list[:idx_1 + 1]
 
-    if command[0] == "first" or command[0] == "last":
+    elif command[0] == "max":
+        idx_2 = command[1]
+        index = None
+        number = - maxsize
+        if idx_2 == "odd":
+            for i in range(len(my_list)):
+                if my_list[i] % 2 != 0 and my_list[i] >= number:
+                    index = i
+                    number = my_list[i]
+        elif idx_2 == "even":
+            for i in range(len(my_list)):
+                if my_list[i] % 2 == 0 and my_list[i] >= number:
+                    index = i
+                    number = my_list[i]
+
+        if index or index == 0:
+            print(index)
+        else:
+            print("No matches")
+
+    elif command[0] == "min":
+        idx_2 = command[1]
+        index = None
+        number = maxsize
+        if idx_2 == "odd":
+            for i in range(len(my_list)):
+                if my_list[i] % 2 != 0 and my_list[i] <= number:
+                    index = i
+                    number = my_list[i]
+        elif idx_2 == "even":
+            for i in range(len(my_list)):
+                if my_list[i] % 2 == 0 and my_list[i] <= number:
+                    index = i
+                    number = my_list[i]
+
+        if index or index == 0:
+            print(index)
+        else:
+            print("No matches")
+
+    elif command[0] == "first":
         numbers_count = int(command[1])
         idx_3 = command[2]
+        even_odd_list = []
+        if numbers_count > len(my_list):
+            print("Invalid count")
+            continue
+
         if idx_3 == "odd":
             for i in my_list:
                 if i % 2 != 0:
                     even_odd_list.append(i)
+                if len(even_odd_list) == numbers_count:
+                    break
+
         elif idx_3 == "even":
             for i in my_list:
                 if i % 2 == 0:
                     even_odd_list.append(i)
+                if len(even_odd_list) == numbers_count:
+                    break
 
-        if 0 <= len(even_odd_list) > numbers_count:
-            if command[0] == "first":
-                even_odd_list = even_odd_list[:numbers_count]
-                print(even_odd_list)
-            else:
-                even_odd_list = even_odd_list[numbers_count:]
-                print(even_odd_list)
-        elif len(even_odd_list) < numbers_count:
+        if even_odd_list:
             print(even_odd_list)
         else:
+            print("[]")
+
+    elif command[0] == "last":
+        numbers_count = int(command[1])
+        idx_3 = command[2]
+        even_odd_list = []
+        if numbers_count > len(my_list):
             print("Invalid count")
+            continue
+
+        if idx_3 == "odd":
+            for i in my_list:
+                if i % 2 != 0:
+                    even_odd_list.append(i)
+                if len(even_odd_list) == numbers_count:
+                    break
+
+        elif idx_3 == "even":
+            for i in my_list:
+                if i % 2 == 0:
+                    even_odd_list.append(i)
+                if len(even_odd_list) == numbers_count:
+                    break
+
+        if even_odd_list:
+            print(even_odd_list[-numbers_count:])
+        else:
+            print("[]")
 
 print(my_list)
-
-
-
