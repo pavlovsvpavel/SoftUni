8bc8d956e7d5de36a622b1a481cf59580849f396
Pavel Pavlov <pavelsvpavlov@gmail.com>
2023-01-29 14:30:49 +0200

    code optimization

--- python_fundamentals_jan_2023/lists_basics/more_exercises/messaging.py
+++ python_fundamentals_jan_2023/lists_basics/more_exercises/messaging.py
@@ -5,11 +5,9 @@ text_length = len(characters)
 current_number = []
 message = []
 for number in sequence:
-    sum_of_numbers = 0
     current_number = [int(x) for x in number]
 
-    for num in current_number:
-        sum_of_numbers += num
+    sum_of_numbers = sum(current_number)
 
     if sum_of_numbers < text_length:
         message.append(characters.pop(sum_of_numbers))
