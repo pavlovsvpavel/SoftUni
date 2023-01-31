5c8a2161ac61c576ec6af417768243bc1325bc7e
Pavel Pavlov <pavelsvpavlov@gmail.com>
2023-01-31 14:03:40 +0200

    Create perfect_number.py

---
+++ python_fundamentals_jan_2023/functions/exercises/perfect_number.py
@@ -0,0 +1,14 @@
+def perfect_number(num):
+    divisors_list = []
+    for i in range(1, num):
+        if num % i == 0:
+            divisors_list.append(i)
+
+    if sum(divisors_list) == num:
+        return "We have a perfect number!"
+    else:
+        return "It's not so perfect."
+
+
+number = int(input())
+print(perfect_number(number))
