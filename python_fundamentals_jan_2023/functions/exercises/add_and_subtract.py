2c05ea0af711409fa03455fd38c405522c18acaa
Pavel Pavlov <pavelsvpavlov@gmail.com>
2023-01-31 11:46:00 +0200

    Update add_and_subtract.py

--- python_fundamentals_jan_2023/functions/exercises/add_and_subtract.py
+++ python_fundamentals_jan_2023/functions/exercises/add_and_subtract.py
@@ -1,17 +1,22 @@
-def add_and_subtract(n1, n2, n3):
-    return add_and_subtract(num_1, num_2, num_3)
-
-
 def sum_numbers(n1, n2):
     return n1 + n2
 
 
-def subtract(n3):
-    return sum_numbers(n1=num_1, n2=num_2) - n3
+def subtract(res, n3):
+    return res - n3
+
+
+def add_and_subtract(n1, n2, n3):
+    sum_of_n1_and_n_2 = sum_numbers(n1, n2)
+    result = subtract(sum_of_n1_and_n_2, n3)
+    return print(result)
 
 
 num_1 = int(input())
 num_2 = int(input())
 num_3 = int(input())
+add_and_subtract(num_1, num_2, num_3)
 
-print(subtract(num_3))
+
+
+
