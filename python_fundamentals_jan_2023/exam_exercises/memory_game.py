3e7eda891ffd540f2902c70d9f4229494c952235
Pavel Pavlov <pavelsvpavlov@gmail.com>
2023-02-04 01:02:21 +0200

    Update memory_game.py

--- python_fundamentals_jan_2023/exam_exercises/memory_game.py
+++ python_fundamentals_jan_2023/exam_exercises/memory_game.py
@@ -10,7 +10,7 @@ def cheat_check(lst, el_lst):
         is_cheat = True
     elif lst[0] < 0 or lst[1] < 0:
         is_cheat = True
-    elif lst[0] > len(elements_lst) - 1 or lst[1] > len(elements_lst) - 1:
+    elif lst[0] > len(el_lst) - 1 or lst[1] > len(el_lst) - 1:
         is_cheat = True
     return is_cheat
 
@@ -25,26 +25,29 @@ def win_lose(lst):
 elements = input()
 command = input()
 elements_lst = [x for x in elements.split(" ")]
-command_lst = [int(x) for x in command.split(" ")]
 moves = 0
 
-if cheat_check(command_lst, elements_lst):
-    moves += 1
-    elements_lst = elements_lst[:(len(elements_lst) // 2) + 1] + 2 * [f"-{moves}a"] + elements_lst[
-                                                                                    (len(elements_lst) // 2) + 1:]
-elif check_for_match(elements_lst):
-    moves += 1
-    print(f"Congrats! You have found matching elements - {check_for_match(elements_lst)}!")
-    elements_lst.pop(command_lst[0])
-    elements_lst.pop(command_lst[1])
-else:
-    moves += 1
-    print("Try again!")
+while command != "end":
+    command_lst = [int(x) for x in command.split(" ")]
 
-if win_lose(elements_lst):
-    print(f"You have won in {moves} turns!")
-else:
+    if cheat_check(command_lst, elements_lst):
+        moves += 1
+        elements_lst = elements_lst[:(len(elements_lst) // 2)] + 2 * [f"-{moves}a"] + elements_lst[
+                                                                                        (len(elements_lst) // 2):]
+        print("Invalid input! Adding additional elements to the board")
+    elif check_for_match(elements_lst):
+        moves += 1
+        print(f"Congrats! You have found matching elements - {check_for_match(elements_lst)}!")
+        elements_lst = list(filter(lambda x: x != check_for_match(elements_lst), elements_lst))
+    else:
+        moves += 1
+        print("Try again!")
+    if win_lose(elements_lst):
+        print(f"You have won in {moves} turns!")
+        break
+
+    command = input()
+
+if not win_lose(elements_lst):
     print("Sorry you lose :(")
     print(" ".join(elements_lst))
-
-print(elements_lst)
