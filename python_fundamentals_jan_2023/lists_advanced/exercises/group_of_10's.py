dac3b8cbbe3d4080ded5d7ad30eb2eb0844c0bca
Pavel Pavlov <pavelsvpavlov@gmail.com>
2023-02-07 17:54:53 +0200

    Create group_of_10's.py

---
+++ python_fundamentals_jan_2023/lists_advanced/exercises/group_of_10's.py
@@ -0,0 +1,21 @@
+numbers = input().split(", ")
+num_list = list(map(int, numbers))
+
+start = 1
+end = 10
+
+while True:
+    if not num_list:
+        break
+
+    current_group = []
+    for num in range(start, end + 1):
+        current_group = list(filter(lambda x: x <= num, num_list))
+
+    for number in current_group:
+        num_list.remove(number)
+
+    print(f"Group of {end}'s: {current_group}")
+
+    start = 1 + end
+    end += 10
