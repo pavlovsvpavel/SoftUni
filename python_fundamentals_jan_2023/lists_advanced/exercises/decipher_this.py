41b800da820d338c5b2d12776dccedebfb91630d
Pavel Pavlov <pavelsvpavlov@gmail.com>
2023-02-08 21:28:59 +0200

    Update decipher_this.py

--- python_fundamentals_jan_2023/lists_advanced/exercises/decipher_this.py
+++ python_fundamentals_jan_2023/lists_advanced/exercises/decipher_this.py
@@ -7,7 +7,8 @@ for word in secret_message:
     digits_to_letter = chr(int(digits_in_word))
     deciphered_message.append(digits_to_letter)
 
-    letters_word = list(filter(lambda x: x.isalpha(), word))
+    # letters_word = list(filter(lambda x: x.isalpha(), word))
+    letters_word = [x for x in word if x.isalpha()]
     deciphered_message.extend(letters_word)
     deciphered_message[1], deciphered_message[-1] = deciphered_message[-1], deciphered_message[1]
 
