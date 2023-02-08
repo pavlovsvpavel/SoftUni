6a81fe6ff221ff6850cc2964412ffe96b8b0f0be
Pavel Pavlov <pavelsvpavlov@gmail.com>
2023-02-08 21:48:48 +0200

    Update which_are_in.py

--- python_fundamentals_jan_2023/lists_advanced/exercises/which_are_in.py
+++ python_fundamentals_jan_2023/lists_advanced/exercises/which_are_in.py
@@ -7,5 +7,6 @@ def check_word(word, lst):
 string_1 = input().split(", ")
 string_2 = input().split(", ")
 
-result = list(filter(lambda x: check_word(x, string_2), string_1))
+# result = list(filter(lambda x: check_word(x, string_2), string_1))
+result = [check_word(x, string_2) for x in string_1 if check_word(x, string_2) is not None]
 print(result)
