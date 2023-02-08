d31e52188d34d7ffbe1c9e55230fa1c93e77c04c
Pavel Pavlov <pavelsvpavlov@gmail.com>
2023-02-08 12:03:45 +0200

    Update anonymous_threat.py

--- python_fundamentals_jan_2023/lists_advanced/exercises/anonymous_threat.py
+++ python_fundamentals_jan_2023/lists_advanced/exercises/anonymous_threat.py
@@ -8,10 +8,15 @@ def merge(lst, s_idx, e_idx):
 def divide(lst, idx, parts):
     result = []
     current_word = "".join([lst[idx]])
+    if parts == 0:
+        result = current_word
+        return result
+
     word_length = len(lst[idx])
     count_char = int(word_length // parts)  # How many letters in one element
     left_parts = int(word_length % parts)   # Remaining elements with left letters
     element_to_add = ""
+    last_part = word_length - count_char - left_parts  # Last part starting index
     if left_parts == 0:
         for i in range(word_length):
             element_to_add += current_word[i]
@@ -22,20 +27,20 @@ def divide(lst, idx, parts):
                 continue
     else:
         for k in range(word_length - 1 - left_parts):
-            last_index = k
             element_to_add += current_word[k]
             if len(element_to_add) == count_char:
                 result.append(element_to_add)
                 element_to_add = ""
             else:
                 continue
-        element_to_add = current_word[last_index:]
+
+        element_to_add = current_word[last_part:]
         result.append(element_to_add)
     return " ".join(result)
 
 
 strings = input().split(" ")
-new_strings = []
+
 while True:
     command = input().split(" ")
     current_command = command[0]
@@ -50,11 +55,10 @@ while True:
         if end_index >= len(strings):
             end_index = len(strings) - 1
         strings = strings[:start_index] + [merge(strings, start_index, end_index)] + strings[end_index + 1:]
-        # print(strings)
+
     elif current_command == "divide":
         index = int(command[1])
         partitions = int(command[2])
         strings = strings[:index] + [divide(strings, index, partitions)] + strings[index + 1:]
-        print(strings)
 
 print(" ".join(strings))
