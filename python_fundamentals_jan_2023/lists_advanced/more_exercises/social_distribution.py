70ae5f91c49c6dd1ba480653e3f1625f5d36ddb8
Pavel Pavlov <pavelsvpavlov@gmail.com>
2023-02-13 23:45:14 +0200

    Update social_distribution.py

--- python_fundamentals_jan_2023/lists_advanced/more_exercises/social_distribution.py
+++ python_fundamentals_jan_2023/lists_advanced/more_exercises/social_distribution.py
@@ -1,31 +1,33 @@
-def difference_between(lst, wealth):
-    diff = 0
+def difference_below_min_wealth(lst, wealth):
+    total_diff = 0
     for element in lst:
         if element < wealth:
-            diff += wealth - element
-    return diff
+            total_diff += wealth - element
+    return total_diff
 
 
-def wealth_distribution(lst, wealth):
-    result = []
-    for el in lst:
-        if el < wealth:
-            el += wealth - el
-            result.append(el)
-        elif el == max(lst):
-            el = lst[-1] - difference_between(population_lst, min_wealth)
-            result.append(el)
-        else:
-            result.append(el)
-
-    return result
+def difference_above_min_wealth(lst, wealth):
+    total_diff = 0
+    for element in lst:
+        if element > wealth:
+            total_diff += element - wealth
+    return total_diff
 
 
 population = input().split(", ")
 min_wealth = int(input())
 population_lst = [int(x) for x in population]
 
-if difference_between(population_lst, min_wealth) + min_wealth <= max(population_lst):
-    print(wealth_distribution(population_lst, min_wealth))
+for idx in range(len(population_lst)):
+    current_num = population_lst[idx]
+    if current_num < min_wealth:
+        diff = min_wealth - current_num
+        current_num += diff
+        population_lst[idx] = current_num
+        max_num_idx = population_lst.index(max(population_lst))
+        population_lst[max_num_idx] -= diff
+
+if difference_above_min_wealth(population_lst, min_wealth) >= difference_below_min_wealth(population_lst, min_wealth):
+    print(population_lst)
 else:
     print("No equal distribution possible")
