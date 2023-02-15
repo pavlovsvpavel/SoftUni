c2a8a637a64e18897c58959d76f64d57c9dec63d
Pavel Pavlov <pavelsvpavlov@gmail.com>
2023-02-15 20:12:39 +0200

    update code

--- python_fundamentals_jan_2023/mid_exam_exercises/memory_game.py
+++ python_fundamentals_jan_2023/mid_exam_exercises/memory_game.py
@@ -29,17 +29,14 @@ moves = 0
 
 while command != "end":
     command_lst = [int(x) for x in command.split(" ")]
-
+    moves += 1
     if cheat_check(command_lst, ele_lst):
-        moves += 1
         ele_lst = ele_lst[:(len(ele_lst) // 2)] + 2 * [f"-{moves}a"] + ele_lst[(len(ele_lst) // 2):]
         print("Invalid input! Adding additional elements to the board")
     elif check_for_match(ele_lst):
-        moves += 1
         print(f"Congrats! You have found matching elements - {check_for_match(ele_lst)}!")
         ele_lst = list(filter(lambda x: x != check_for_match(ele_lst), ele_lst))
     else:
-        moves += 1
         print("Try again!")
     if win_lose(ele_lst):
         print(f"You have won in {moves} turns!")
