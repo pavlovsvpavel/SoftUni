c3d480ccc2f6e1fe1fd8b0cd8acdf61216810a77
Pavel Pavlov <pavelsvpavlov@gmail.com>
2023-02-26 17:50:23 +0200

    Create statistics.py

---
+++ python_fundamentals_jan_2023/dictionaries/lab/statistics.py
@@ -0,0 +1,23 @@
+products = {}
+while True:
+    command = input()
+
+    if command == "statistics":
+        break
+
+    items = command.split(": ")
+    product = items[0]
+    quantity = items[1]
+
+    if product not in products:
+        products[product] = 0
+
+    products[product] += int(quantity)
+
+print("Products in stock:")
+for product, quantity in products.items():
+    print(f"- {product}: {quantity}")
+
+print(f"Total Products: {len(products)}")
+print(f"Total Quantity: {sum(products.values())}")
+
