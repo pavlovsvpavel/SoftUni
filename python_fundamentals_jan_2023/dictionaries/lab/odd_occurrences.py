860748bddd00ff7a4ed31f6d0d7e801ecf2bec67
Pavel Pavlov <pavelsvpavlov@gmail.com>
2023-03-01 21:17:35 +0200

    Update odd_occurrences.py

--- python_fundamentals_jan_2023/dictionaries/lab/odd_occurrences.py
+++ python_fundamentals_jan_2023/dictionaries/lab/odd_occurrences.py
@@ -3,9 +3,9 @@ dictionary = {}
 
 for element in elements:
     element_lower = element.lower()
-    if element_lower not in dictionary.keys():
-        dictionary[element_lower] = 0
-    dictionary[element_lower] += 1
+    # if element_lower not in dictionary.keys():
+    #     dictionary[element_lower] = 0
+    dictionary[element_lower] = dictionary.get(element_lower, 0) + 1
 
 for key, value in dictionary.items():
     if value % 2 != 0:
