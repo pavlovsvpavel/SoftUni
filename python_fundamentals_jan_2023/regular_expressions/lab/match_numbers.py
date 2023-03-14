52b4c050414f9ff425ad73da575ccaf9188bc36a
Pavel Pavlov <pavelsvpavlov@gmail.com>
2023-03-14 20:04:20 +0200

    Create match_numbers.py

---
+++ python_fundamentals_jan_2023/regular_expressions/lab/match_numbers.py
@@ -0,0 +1,10 @@
+import re
+
+text = input()
+
+pattern = r"(^|(?<=\s))-?([0]|[1-9]\d*)(\.\d+)?($|(?=\s))"
+
+result = re.finditer(pattern, text)
+
+for match in result:
+    print(match.group(), end=" ")
