c52866edddbfac71d1b108e74cdfab803a959ba3
Pavel Pavlov <pavelsvpavlov@gmail.com>
2023-03-17 21:50:15 +0200

    Update extract_the_links.py

--- python_fundamentals_jan_2023/regular_expressions/exercises/extract_the_links.py
+++ python_fundamentals_jan_2023/regular_expressions/exercises/extract_the_links.py
@@ -3,11 +3,12 @@ import re
 while True:
     input_text = input()
 
-    pattern = r"www\.[a-zA-Z0-9\-]+\.[\.a-z]+"
+    pattern = r"(www\.[a-zA-Z0-9\-]+(\.[a-z]+)+)"
 
     if input_text:
-        result = re.findall(pattern, input_text)
+        result = re.finditer(pattern, input_text)
         if result:
-            print("".join(result))
+            for el in result:
+                print("".join(el[0]))
     else:
         break
