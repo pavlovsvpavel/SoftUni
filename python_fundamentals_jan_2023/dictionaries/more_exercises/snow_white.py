9b72e1241ef65c200458815b012980bc917686fa
Pavel Pavlov <pavelsvpavlov@gmail.com>
2023-03-31 16:43:26 +0300

    Update snow_white.py

--- python_fundamentals_jan_2023/dictionaries/more_exercises/snow_white.py
+++ python_fundamentals_jan_2023/dictionaries/more_exercises/snow_white.py
@@ -1,5 +1,4 @@
 dwarfs = {}
-flag = False
 while True:
     line = input()
     if line == "Once upon a time":
@@ -10,29 +9,16 @@ while True:
     name = line_args[0]
     color = line_args[1]
     physics = int(line_args[2])
-    dwarfs[name] = dwarfs.get(name, {})
-    dwarfs[name][color] = dwarfs[name].get(color, 0)
-    if physics > dwarfs[name][color]:
-        dwarfs[name][color] = physics
+    dwarfs[color] = dwarfs.get(color, {})
+    dwarfs[color][name] = dwarfs[color].get(name, 0)
+    if physics > dwarfs[color][name]:
+        dwarfs[color][name] = physics
 
-count_colors = {}
-for key, value in dwarfs.items():
-    for color in value.keys():
-        if color in count_colors:
-            count_colors[color] += 1
-        else:
-            count_colors[color] = count_colors.get(color, 1)
-for value in count_colors.values():
-    if value > 1:
-        flag = True
-
-if flag:
-    for hat_color, value in sorted(count_colors.items(), key=lambda x: (-x[1], x[0])):
-        for dwarf in dwarfs.keys():
-            for name, points in sorted(dwarfs[dwarf].items(), key=lambda x: (-x[1], x[0])):
-                if hat_color == name:
-                    print(f"({name}) {dwarf} <-> {points}")
-else:
-    for dwarf, value in dwarfs.items():
-        for name, points in sorted(value.items(), key=lambda x: (-x[1], x[0])):
-            print(f"({name}) {dwarf} <-> {points}")
+dwarfs_list = []
+for hat_color in dwarfs.keys():
+    for dwarf_name, points in dwarfs[hat_color].items():
+        dwarfs_list.append({
+            "length": len(dwarfs[hat_color]), "hat_color": hat_color, "dwarf_name": dwarf_name, "points": points
+        })
+for result in sorted(dwarfs_list, key=lambda x: (-x["points"], -x["length"])):
+    print(f"({result['hat_color']}) {result['dwarf_name']} <-> {result['points']}")
