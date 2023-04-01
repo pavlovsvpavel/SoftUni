058ec42b937a37891006431aaceb81904420cd30
Pavel Pavlov <pavelsvpavlov@gmail.com>
2023-04-01 12:09:26 +0300

    Update treasure_finder.py

--- python_fundamentals_jan_2023/text_processing/more_exercises/treasure_finder.py
+++ python_fundamentals_jan_2023/text_processing/more_exercises/treasure_finder.py
@@ -13,10 +13,11 @@ while True:
         new_char = ord(char) - key[index]
         decrypted_message += chr(new_char)
         index += 1
-    type_pattern = r"&([A-Za-z]+)&"
-    coordinates_pattern = r"<([A-Za-z\d]+)>"
-
-    treasure_type = re.findall(type_pattern, decrypted_message)
-    coordinates = re.findall(coordinates_pattern, decrypted_message)
-
-    print(f"Found {treasure_type[0]} at {coordinates[0]}")
+    # type_pattern = r"&([A-Za-z]+)&"
+    # coordinates_pattern = r"<([A-Za-z\d]+)>"
+    # treasure_type = re.findall(type_pattern, decrypted_message)
+    # coordinates = re.findall(coordinates_pattern, decrypted_message)
+    # print(f"Found {treasure_type[0]} at {coordinates[0]}")
+    pattern = r"&([A-Za-z]+)&(\w+)<([A-Za-z\d]+)>"
+    matches = re.findall(pattern, decrypted_message)
+    print(f"Found {matches[0][0]} at {matches[0][2]}")
