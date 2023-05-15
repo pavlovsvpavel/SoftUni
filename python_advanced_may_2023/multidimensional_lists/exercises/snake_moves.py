cd40e56fa0f4370a35d7a06a5dcb52c6c88dc3f6
Pavel Pavlov <pavelsvpavlov@gmail.com>
2023-05-15 17:24:00 +0300

    Update snake_moves.py

--- python_advanced_may_2023/multidimensional_lists/exercises/snake_moves.py
+++ python_advanced_may_2023/multidimensional_lists/exercises/snake_moves.py
@@ -9,17 +9,16 @@ def print_path(matrix):
 
 
 rows, cols = [int(x) for x in input().split(" ")]
-snake = input()
+snake = deque(input())
 
 matrix = []
-snake_copy = deque(snake)
 
 for row in range(rows):
     row_data = []
     for col in range(cols + 1):
-        current_el = snake_copy.popleft()
+        current_el = snake.popleft()
         row_data.append(current_el)
-        snake_copy.append(current_el)
+        snake.append(current_el)
 
         if len(row_data) == cols:
             matrix.append(row_data)
