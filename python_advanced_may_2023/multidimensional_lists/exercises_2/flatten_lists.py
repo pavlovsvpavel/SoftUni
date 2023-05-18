8ccdebdbd4f282a091da393400c924c885dc4c3f
Pavel Pavlov <pavelsvpavlov@gmail.com>
2023-05-18 18:53:04 +0300

    Update flatten_lists.py

--- python_advanced_may_2023/multidimensional_lists/exercises_2/flatten_lists.py
+++ python_advanced_may_2023/multidimensional_lists/exercises_2/flatten_lists.py
@@ -1,8 +1,7 @@
-data = [x.split(" ") for x in input().split("|")]
+data = [x.split() for x in input().split("|")]
 
 for row in range(len(data) - 1, - 1, - 1):
     for col in range(len(data[row])):
         current_el = data[row][col]
-        if current_el:
-            print(current_el, end=" ")
+        print(current_el, end=" ")
 
