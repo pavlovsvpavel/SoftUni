bb1e7c5cdba0e561eb11795f9ea20f5ada102846
Pavel Pavlov <pavelsvpavlov@gmail.com>
2023-05-23 11:43:19 +0300

    Create rectangle.py

---
+++ functions_advanced/lab/rectangle.py
@@ -0,0 +1,16 @@
+def rectangle(length, width):
+    if type(length) != int or type(width) != int:
+        return "Enter valid values!"
+
+    def area():
+        return length * width
+
+    def perimeter():
+        return (length + width) * 2
+
+    return f"Rectangle area: {area()}\nRectangle perimeter: {perimeter()}"
+
+
+print(rectangle(2, 10))
+
+print(rectangle('2', 10))
