79810272672bbbf586f7c0de66009bce31082cc3
Pavel Pavlov <pavelsvpavlov@gmail.com>
2023-05-25 19:12:13 +0300

    Create numbers_dictionary.py

---
+++ python_advanced_may_2023/error_handling/exercises/numbers_dictionary.py
@@ -0,0 +1,38 @@
+numbers_dictionary = {}
+
+while True:
+    line = input()
+
+    if line == "Search":
+        break
+
+    try:
+        number_as_string = line
+        number = int(input())
+        numbers_dictionary[number_as_string] = number
+    except ValueError:
+        print("The variable number must be an integer")
+
+while True:
+    line = input()
+
+    if line == "Remove":
+        break
+
+    searched = line
+    print(numbers_dictionary[searched])
+
+while True:
+    line = input()
+
+    if line == "End":
+        break
+
+    searched = line
+
+    try:
+        del numbers_dictionary[searched]
+    except KeyError:
+        print("Number does not exist in dictionary")
+
+print(numbers_dictionary)
