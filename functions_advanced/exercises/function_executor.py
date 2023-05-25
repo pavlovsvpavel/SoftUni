a0e8846c8e8b5b7ef98dea6540171d78fe0f9d55
Pavel Pavlov <pavelsvpavlov@gmail.com>
2023-05-25 10:45:12 +0300

    Create function_executor.py

---
+++ functions_advanced/exercises/function_executor.py
@@ -0,0 +1,21 @@
+def func_executor(*args):
+    result = []
+
+    for func, values in args:
+        result.append(f"{func.__name__} - {func(*values)}")
+
+    return '\n'.join(result)
+
+
+def sum_numbers(num1, num2):
+    return num1 + num2
+
+
+def multiply_numbers(num1, num2):
+    return num1 * num2
+
+
+print(func_executor(
+    (sum_numbers, (1, 2)),
+    (multiply_numbers, (2, 4))
+))
