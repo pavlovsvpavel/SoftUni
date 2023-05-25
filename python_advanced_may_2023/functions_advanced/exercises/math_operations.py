71a7cab817a3f898c6408361cc88bbd922706282
Pavel Pavlov <pavelsvpavlov@gmail.com>
2023-05-25 15:08:49 +0300

    Create math_operations.py

---
+++ python_advanced_may_2023/functions_advanced/exercises/math_operations.py
@@ -0,0 +1,35 @@
+from collections import deque
+
+
+def math_operations(*args, **kwargs):
+    numbers = deque(float(num) for num in args)
+    result = []
+
+    while numbers:
+        for operator, value in kwargs.items():
+            current_num = numbers.popleft()
+
+            if operator == "a":
+                kwargs[operator] += current_num
+            elif operator == "s":
+                kwargs[operator] -= current_num
+            elif operator == "d":
+                if current_num != 0:
+                    kwargs[operator] /= current_num
+            elif operator == "m":
+                kwargs[operator] *= current_num
+
+            if not numbers:
+                break
+
+    for key, value in sorted(kwargs.items(), key=lambda x: (-x[1], x[0])):
+        result.append(f"{key}: {value:.1f}")
+
+    return "\n".join(result)
+
+
+print(math_operations(2.1, 12.56, 0.0, -3.899, 6.0, -20.65, a=1, s=7, d=33, m=15))
+
+print(math_operations(-1.0, 0.5, 1.6, 0.5, 6.1, -2.8, 80.0, a=0, s=(-2.3), d=0, m=0))
+
+
