3827c6f5e94accada94f3ed3fbc7d525d46c94c1
Pavel Pavlov <pavelsvpavlov@gmail.com>
2023-05-26 20:58:11 +0300

    Update email_validator.py

--- python_advanced_may_2023/error_handling/exercises/email_validator.py
+++ python_advanced_may_2023/error_handling/exercises/email_validator.py
@@ -13,20 +13,28 @@ class InvalidDomainError(Exception):
     pass
 
 
+class MustContainOnlyOneAtSymbolError(Exception):
+    pass
+
+
+name_pattern = r"\w{5,}"
+domain_pattern = r"\.com$|\.bg$|\.net$|\.org$"
+
 email = input()
 
 while email != "End":
     symbol_idx = [email.index(el) for el in email if el == "@"]
+    name_validator = bool(re.search(name_pattern, email))
+    domain_validator = bool(re.search(domain_pattern, email))
 
-    if not symbol_idx:
-        raise MustContainAtSymbolError("MustContainAtSymbolError")
+    if not name_validator:
+        raise NameTooShortError("Name must be more than 4 characters")
 
-    name = email[:symbol_idx[0]]
-    pattern = r"\.com$|\.bg$|\.net$|\.org$"
-    domain_validator = bool(re.search(pattern, email))
+    elif len(symbol_idx) > 1:
+        raise MustContainOnlyOneAtSymbolError("Email must contain only one '@' symbol")
 
-    if len(name) < 5:
-        raise NameTooShortError("NameTooShortError")
+    elif not symbol_idx:
+        raise MustContainAtSymbolError("Email must contain @")
 
     elif not domain_validator:
         raise InvalidDomainError("Domain must be one of the following: .com, .bg, .org, .net")
