d051c7151bf21540c4d89c3e79dc9ab7f5369619
Pavel Pavlov <pavelsvpavlov@gmail.com>
2023-05-27 13:26:42 +0300

    solved problems

--- python_advanced_may_2023/file_handling/lab/word_count.py
+++ python_advanced_may_2023/file_handling/lab/word_count.py
@@ -1,1 +1,19 @@
-new_file = open("words.txt", "w")
+import re
+words_dict = {}
+
+pattern = r"[a-zA-Z]+"
+with open("input.txt") as f:
+    matches = re.findall(pattern, f.read().lower())
+
+with open("words.txt") as f:
+    words = f.read().lower()
+
+    for word in words.split():
+        counter = matches.count(word)
+        words_dict[word] = counter
+
+words_sorted = sorted(words_dict.items(), key=lambda x: -x[1])
+
+with open("output.txt", "w") as f:
+    for word, counts in words_sorted:
+        f.write(f"{word} - {counts}\n")
