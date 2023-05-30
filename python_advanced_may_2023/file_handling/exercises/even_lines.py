3318511e189beeec217c402e0ee4f5b85ef34cec
Pavel Pavlov <pavelsvpavlov@gmail.com>
2023-05-30 23:41:19 +0300

    update

--- python_advanced_may_2023/file_handling/exercises/even_lines.py
+++ python_advanced_may_2023/file_handling/exercises/even_lines.py
@@ -3,7 +3,7 @@ import re
 even_lines = []
 pattern = r"-|,|, |\.|\!|\?"
 
-with open("files/text.txt", "r") as file:
+with open("text.txt", "r") as file:
     for i, line in enumerate(file):
         if i % 2 == 0:
             current_line = line.strip()
