6eb8171b63eb76c7496e9e4215a38b623685b477
Pavel Pavlov <pavelsvpavlov@gmail.com>
2023-06-05 12:02:34 +0300

    update

--- python_advanced_may_2023/exam_exercises/song_creator.py
+++ python_advanced_may_2023/exam_exercises/song_creator.py
@@ -16,25 +16,25 @@ def add_songs(*args):
     return "\n".join(result)
 
 
-# print(add_songs(
-#     ("Bohemian Rhapsody", []),
-#     ("Just in Time",
-#      ["Just in time, I found you just in time",
-#       "Before you came, my time was running low",
-#       "I was lost, the losing dice were tossed",
-#       "My bridges all were crossed, nowhere to go"])
-# ))
+print(add_songs(
+    ("Bohemian Rhapsody", []),
+    ("Just in Time",
+     ["Just in time, I found you just in time",
+      "Before you came, my time was running low",
+      "I was lost, the losing dice were tossed",
+      "My bridges all were crossed, nowhere to go"])
+))
 
-# print(add_songs(
-#     ("Beat It", []),
-#     ("Beat It",
-#      ["Just beat it (beat it), beat it (beat it)",
-#       "No one wants to be defeated"]),
-#     ("Beat It", []),
-#     ("Beat It",
-#      ["Showin' how funky and strong is your fight",
-#       "It doesn't matter who's wrong or right"]),
-# ))
+print(add_songs(
+    ("Beat It", []),
+    ("Beat It",
+     ["Just beat it (beat it), beat it (beat it)",
+      "No one wants to be defeated"]),
+    ("Beat It", []),
+    ("Beat It",
+     ["Showin' how funky and strong is your fight",
+      "It doesn't matter who's wrong or right"]),
+))
 
 print(add_songs(
     ("Love of my life",
