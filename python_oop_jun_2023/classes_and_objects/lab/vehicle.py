e0fa412d1f06c6924b62b7ead79cfb0eabbd7d1e
Pavel Pavlov <pavelsvpavlov@gmail.com>
2023-06-25 19:17:44 +0300

    solved problems

---
+++ python_oop_jun_2023/classes_and_objects/lab/vehicle.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Vehicle:
+    def __init__(self, mileage: float, max_speed: float = 150):
+        self.mileage = mileage
+        self.max_speed = max_speed
+        self.gadgets: List[str] = []
+
+
+car = Vehicle(20)
+print(car.max_speed)
+print(car.mileage)
+print(car.gadgets)
+car.gadgets.append('Hudly Wireless')
+print(car.gadgets)
