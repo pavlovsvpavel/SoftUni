e4df6e7a9f35d6338dca84cb126e91aba403644d
Pavel Pavlov <pavelsvpavlov@gmail.com>
2023-06-25 22:36:05 +0300

    solved problems

---
+++ python_oop_jun_2023/classes_and_objects/exercises/pizza_delivery.py
@@ -0,0 +1,59 @@
+from typing import Dict
+
+
+class PizzaDelivery:
+    ordered = False
+
+    def __init__(self, name: str, price: float, ingredients: Dict):
+        self.name = name
+        self.price = price
+        self.ingredients = ingredients
+
+    def add_extra(self, ingredient: str, quantity: int, price_per_quantity: float) -> str or None:
+        if PizzaDelivery.ordered:
+            return f"Pizza {self.name} already prepared, and we can't make any changes!"
+
+        if ingredient in self.ingredients:
+            self.ingredients[ingredient] += quantity
+        else:
+            self.ingredients[ingredient] = quantity
+
+        self.price += price_per_quantity * quantity
+
+    def remove_ingredient(self, ingredient: str, quantity: int, price_per_quantity: float) -> str:
+        if PizzaDelivery.ordered:
+            return f"Pizza {self.name} already prepared, and we can't make any changes!"
+
+        try:
+            if quantity > self.ingredients[ingredient]:
+                quantity = str(quantity)
+
+            self.ingredients[ingredient] -= quantity
+            self.price -= price_per_quantity * quantity
+
+        except KeyError:
+            return f"Wrong ingredient selected! We do not use {ingredient} in {self.name}!"
+
+        except TypeError:
+            return f"Please check again the desired quantity of {ingredient}!"
+
+    def make_order(self) -> str:
+        PizzaDelivery.ordered = True
+        result = []
+
+        for ingredient, qty in self.ingredients.items():
+            result.append(f"{ingredient}: {qty}")
+
+        return f"You've ordered pizza {self.name} prepared with " \
+               f"{', '.join(result)} and the price will be {self.price}lv."
+
+
+margarita = PizzaDelivery('Margarita', 11, {'cheese': 2, 'tomatoes': 1})
+margarita.add_extra('mozzarella', 1, 0.5)
+margarita.add_extra('cheese', 1, 1)
+margarita.remove_ingredient('cheese', 1, 1)
+print(margarita.remove_ingredient('bacon', 1, 2.5))
+print(margarita.remove_ingredient('tomatoes', 2, 0.5))
+margarita.remove_ingredient('cheese', 2, 1)
+print(margarita.make_order())
+print(margarita.add_extra('cheese', 1, 1))
