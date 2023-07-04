23587dbc209e95d37d143df22b81ea21ce1c6f78
Pavel Pavlov <pavelsvpavlov@gmail.com>
2023-07-04 18:43:11 +0300

    code update

--- python_oop_jun_2023/class_and_static_methods/exercises/photo_album.py
+++ python_oop_jun_2023/class_and_static_methods/exercises/photo_album.py
@@ -7,19 +7,24 @@ class PhotoAlbum:
 
     def __init__(self, pages: int) -> None:
         self.pages = pages
-        self.photos: List = [[] for _ in range(pages)]
+        self.photos: List = self.__album_creation()
+
+    def __album_creation(self):
+        matrix = [[] for _ in range(self.pages)]
+
+        return matrix
 
     @classmethod
     def from_photos_count(cls, photos_count: int):
         return cls(ceil(photos_count / PhotoAlbum.PHOTOS_PER_PAGE))
 
     def add_photo(self, label: str) -> str:
-        for row in range(len(self.photos)):
-            if len(self.photos[row]) < PhotoAlbum.PHOTOS_PER_PAGE:
-                self.photos[row].append(label)
+        for page_num, row in enumerate(self.photos, 1):
+            if len(row) < PhotoAlbum.PHOTOS_PER_PAGE:
+                row.append(label)
 
                 return f"{label} photo added successfully on page " \
-                       f"{row + 1} slot {len(self.photos[row])}"
+                       f"{page_num} slot {row.index(label) + 1}"
 
         return "No more free slots"
 
@@ -50,54 +55,3 @@ print(album.photos)
 print(album.add_photo("prom"))
 print(album.add_photo("wedding"))
 print(album.display())
-
-# import unittest
-#
-#
-# class TestsPhotoAlbum(unittest.TestCase):
-#     def test_init_creates_all_attributes(self):
-#         album = PhotoAlbum(2)
-#         self.assertEqual(album.pages, 2)
-#         self.assertEqual(album.photos, [[], []])
-#
-#     def test_from_photos_should_create_instace(self):
-#         album = PhotoAlbum.from_photos_count(12)
-#         self.assertEqual(album.pages, 3)
-#         self.assertEqual(album.photos, [[], [], []])
-#
-#     def test_add_photo_with_no_free_spots(self):
-#         album = PhotoAlbum(1)
-#         album.add_photo("baby")
-#         album.add_photo("first grade")
-#         album.add_photo("eight grade")
-#         album.add_photo("party with friends")
-#         result = album.add_photo("prom")
-#         self.assertEqual(result, "No more free slots")
-#
-#     def test_add_photo_with_free_spots(self):
-#         album = PhotoAlbum(1)
-#         album.add_photo("baby")
-#         album.add_photo("first grade")
-#         album.add_photo("eight grade")
-#         album.add_photo("party with friends")
-#         self.assertEqual(album.photos, [['baby', 'first grade', 'eight grade', 'party with friends']])
-#
-#     def test_display_with_one_page(self):
-#         album = PhotoAlbum(1)
-#         album.add_photo("baby")
-#         album.add_photo("first grade")
-#         album.add_photo("eight grade")
-#         album.add_photo("party with friends")
-#         result = album.display().strip()
-#         self.assertEqual(result, "-----------\n[] [] [] []\n-----------")
-#
-#     def test_display_with_three_pages(self):
-#         album = PhotoAlbum(3)
-#         for _ in range(8):
-#             album.add_photo("asdf")
-#         result = album.display().strip()
-#         self.assertEqual(result, "-----------\n[] [] [] []\n-----------\n[] [] [] []\n-----------\n\n-----------")
-#
-#
-# if __name__ == "__main__":
-#     unittest.main()
