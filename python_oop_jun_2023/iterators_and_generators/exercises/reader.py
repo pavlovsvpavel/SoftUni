26c57f2631d2cc91e80f37b0a200d5ea03cb0ad7
Pavel Pavlov <pavelsvpavlov@gmail.com>
2023-07-28 19:03:56 +0300

    code update

--- python_oop_jun_2023/iterators_and_generators/exercises/reader.py
+++ python_oop_jun_2023/iterators_and_generators/exercises/reader.py
@@ -1,7 +1,16 @@
+# def read_next(*args):
+#     for seq in args:
+#         for el in seq:
+#             yield el
+#
+#
+# for i in read_next("Need", (2, 3), ["words", "."]):
+#     print(i)
+
+
 def read_next(*args):
     for seq in args:
-        for el in seq:
-            yield el
+        yield from seq
 
 
 for i in read_next("Need", (2, 3), ["words", "."]):
