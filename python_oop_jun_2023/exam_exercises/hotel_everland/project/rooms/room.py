879f6fb2f302653d140ee8cb2a527761ab782044
Pavel Pavlov <pavelsvpavlov@gmail.com>
2023-08-04 15:11:43 +0300

    solved problems

---
+++ python_oop_jun_2023/exam_exercises/hotel_everland/project/rooms/room.py
@@ -0,0 +1,27 @@
+class Room:
+    def __init__(self, name: str, budget: float, members_count: int):
+        self.family_name = name
+        self.budget = budget
+        self.members_count = members_count
+        self.children = []
+        self.expenses = 0
+
+    @property
+    def expenses(self):
+        return self._expenses
+
+    @expenses.setter
+    def expenses(self, value):
+        if value < 0:
+            raise ValueError("Expenses cannot be negative")
+        self._expenses = value
+
+    def calculate_expenses(self, *args):
+        total = 0
+
+        for lst in args:
+            for a in lst:
+                total += a.get_monthly_expense()
+
+        self.expenses = total
+
