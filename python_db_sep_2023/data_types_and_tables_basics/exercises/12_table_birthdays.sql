74f9b9c08e2e691a77643956db040c5f1aa3c50f
Pavel Pavlov <pavelsvpavlov@gmail.com>
2023-09-14 12:32:09 +0300

    solved problems

---
+++ python_db_sep_2023/data_types_and_tables_basics/exercises/12_table_birthdays.sql
@@ -0,0 +1,9 @@
+CREATE TABLE minions_birthdays(
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(150) NOT NULL,
+	date_of_birth DATE NOT NULL,
+	age INTEGER NOT NULL,
+	present VARCHAR(100) NOT NULL,
+	party TIMESTAMPTZ NOT NULL
+)
+
---
+++ python_db_sep_2023/data_types_and_tables_basics/exercises/15_truncate_the_table.sql
@@ -0,0 +1,3 @@
+TRUNCATE TABLE minions_info
+			
+
