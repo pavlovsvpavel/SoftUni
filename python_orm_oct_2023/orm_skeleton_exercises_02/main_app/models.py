0b717698fc477a50bb8a8aabf4a447c75434505d
Pavel Pavlov <pavelsvpavlov@gmail.com>
2023-10-21 09:22:44 +0300

    solved problems

---
+++ python_orm_oct_2023/orm_skeleton_exercises_02/main_app/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Person(models.Model):
+    name = models.CharField(max_length=30)
+    age = models.PositiveIntegerField()
+
+
+class Blog(models.Model):
+    post = models.TextField()
+    author = models.CharField(max_length=35)
+
+
