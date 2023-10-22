a2917b16348f5ff211c7ffebe1a42956bf94feb1
Pavel Pavlov <pavelsvpavlov@gmail.com>
2023-10-22 13:08:03 +0300

    solved problems

--- python_orm_oct_2023/migrations_and_django_admin/exercises/03-exercises-orm-skeleton/caller.py
+++ python_orm_oct_2023/migrations_and_django_admin/exercises/03-exercises-orm-skeleton/caller.py
@@ -6,5 +6,92 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
 # Import your models here
+from main_app.models import Person, Item, Smartphone, Order
 
-# Create queries within functions
+
+def add_products():
+    product1 = Person(
+        name='Ivan',
+        age=25
+    )
+    product1.save()
+
+    product2 = Person(
+        name='Pesho',
+        age=12
+    )
+    product2.save()
+    return 'Products added successfully'
+
+
+def add_items():
+    item1 = Item(
+        name='Joto',
+        price=25,
+        quantity=1000
+    )
+    item1.save()
+
+    item2 = Item(
+        name='JJJ',
+        price=100,
+        quantity=100
+    )
+    item2.save()
+    return 'Items added successfully'
+
+
+def add_smartphones():
+    item1 = Smartphone(
+        brand='Samsung',
+        price=1000
+    )
+    item1.save()
+
+    item2 = Smartphone(
+        brand='iPhone',
+        price=2000
+    )
+    item2.save()
+    return 'Smartphones added successfully'
+
+
+def add_orders():
+    order1 = Order(
+        product_name='Watch',
+        customer_name='Ivan',
+        order_date='2023-10-10',
+        status='Pending',
+        product_price=100,
+        total_price=100
+    )
+    order1.save()
+
+    order2 = Order(
+        product_name='Phone',
+        customer_name='Pesho',
+        order_date='2023-10-22',
+        status='Completed',
+        product_price=1000,
+        total_price=1000
+    )
+    order2.save()
+
+    order3 = Order(
+        product_name='TV',
+        customer_name='Gosho',
+        order_date='2023-10-20',
+        status='Cancelled',
+        product_price=500,
+        total_price=500
+    )
+    order3.save()
+
+    return 'Order added successfully'
+
+
+# print(add_products())
+# print(add_items())
+# print(add_smartphones())
+# print(add_orders())
+
--- python_orm_oct_2023/migrations_and_django_admin/exercises/03-exercises-orm-skeleton/main_app/models.py
+++ python_orm_oct_2023/migrations_and_django_admin/exercises/03-exercises-orm-skeleton/main_app/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
+STATUS = [
+    ('Pending', 'Pending'),
+    ('Comleted', 'Completed'),
+    ('Cancelled', 'Cancelled')
+]
 
 class Shoe(models.Model):
     brand = models.CharField(max_length=25)
@@ -61,3 +66,41 @@ class Course(models.Model):
 
     def __str__(self):
         return f"{self.title} - {self.lecturer}"
+
+
+class Person(models.Model):
+    name = models.CharField(max_length=40)
+    age = models.PositiveIntegerField()
+    age_group = models.CharField(max_length=20, default='No age group')
+
+    def __str__(self):
+        return f"Name {self.name}"
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=0)
+    rarity = models.CharField(max_length=20, default='empty')
+
+
+class Smartphone(models.Model):
+    brand = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    category = models.CharField(max_length=20, default='empty')
+
+
+class Order(models.Model):
+    product_name = models.CharField(max_length=30)
+    customer_name = models.CharField(max_length=100)
+    order_date = models.DateField()
+    status = models.CharField(max_length=30, choices=STATUS)
+    amount = models.PositiveIntegerField(default=0)
+    product_price = models.DecimalField(max_digits=10, decimal_places=2)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    warranty = models.CharField(max_length=30, default='No warranty')
+    delivery = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return f"Order #{self.id} - {self.customer_name}"
+
