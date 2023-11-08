95ce6c014ba400e635658cd45a37d64b7f116fa0
Pavel Pavlov <pavelsvpavlov@gmail.com>
2023-11-08 18:47:22 +0200

    solved problems

--- python_orm_oct_2023/advanced_queries_in_django/exercises/09-skeleton_exercises/caller.py
+++ python_orm_oct_2023/advanced_queries_in_django/exercises/09-skeleton_exercises/caller.py
@@ -5,6 +5,115 @@ import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
-# Import your models
-# Create and check models
-# Run and print your queries
+from main_app.models import RealEstateListing, VideoGame, BillingInfo, Invoice
+
+# Problem 1
+# # Create instances of RealEstateListing with locations
+# RealEstateListing.objects.create(
+#     property_type='House',
+#     price=100000.00,
+#     bedrooms=3,
+#     location='Los Angeles'
+# )
+# RealEstateListing.objects.create(
+#     property_type='Flat',
+#     price=75000.00,
+#     bedrooms=2,
+#     location='New York City'
+# )
+#
+# RealEstateListing.objects.create(
+#     property_type='Villa',
+#     price=250000.00,
+#     bedrooms=4,
+#     location='Los Angeles'  # Same location as the first instance
+# )
+#
+# RealEstateListing.objects.create(
+#     property_type='House',
+#     price=120000.00,
+#     bedrooms=3,
+#     location='San Francisco'
+# )
+#
+# # Run the 'by_property_type' method
+# house_listings = RealEstateListing.objects.by_property_type('House')
+# print("House listings:")
+# for listing in house_listings:
+#     print(f"- {listing.property_type} in {listing.location}")
+#
+# # Run the 'in_price_range' method
+# affordable_listings = RealEstateListing.objects.in_price_range(75000.00, 120000.00)
+# print("Price in range listings:")
+# for listing in affordable_listings:
+#     print(f"- {listing.property_type} in {listing.location}")
+#
+# # Run the 'with_bedrooms' method
+# two_bedroom_listings = RealEstateListing.objects.with_bedrooms(2)
+# print("Two-bedroom listings:")
+# for listing in two_bedroom_listings:
+#     print(f"- {listing.property_type} in {listing.location}")
+#
+# # Run the 'popular_locations' method
+# popular_locations = RealEstateListing.objects.popular_locations()
+# print("Popular locations:")
+# for location in popular_locations:
+#     print(f"- {location['location']}")
+
+# Problem 2
+# Create instances of VideoGame with real data
+# game1 = VideoGame.objects.create(title="The Last of Us Part II", genre="Action", release_year=2020, rating=9.0)
+#
+# game2 = VideoGame.objects.create(title="Cyberpunk 2077", genre="RPG", release_year=2020, rating=7.2)
+#
+# game3 = VideoGame.objects.create(title="Red Dead Redemption 2", genre="Adventure", release_year=2018, rating=9.7)
+#
+# game4 = VideoGame.objects.create(title="FIFA 22", genre="Sports", release_year=2021, rating=8.5)
+#
+# game5 = VideoGame.objects.create(title="Civilization VI", genre="Strategy", release_year=2016, rating=8.8)
+
+# Run the custom manager methods
+# action_games = VideoGame.objects.games_by_genre('Action')
+# recent_games = VideoGame.objects.recently_released_games(2019)
+# average_rating = VideoGame.objects.average_rating()
+# highest_rated = VideoGame.objects.highest_rated_game()
+# lowest_rated = VideoGame.objects.lowest_rated_game()
+#
+# # Print the results
+# print(action_games)
+# print(recent_games)
+# print(average_rating)
+# print(highest_rated)
+# print(lowest_rated)
+
+
+# Problem 3
+# Create BillingInfo instances with real addresses
+# billing_info_1 = BillingInfo.objects.create(address="456 Oak Lane, Boston, MA 02108")
+#
+# billing_info_2 = BillingInfo.objects.create(address="789 Maple Avenue, San Francisco, CA 94101")
+#
+# billing_info_3 = BillingInfo.objects.create(address="101 Pine Street, New York, NY 10001")
+
+# Create Invoice instances with related BillingInfo
+# invoice_1 = Invoice.objects.create(invoice_number="INV007", billing_info=billing_info_1)
+# invoice_2 = Invoice.objects.create(invoice_number="INV002", billing_info=billing_info_2)
+# invoice_3 = Invoice.objects.create(invoice_number="INV004", billing_info=billing_info_3)
+
+# # Get invoices starting with a specific prefix
+# invoices_with_prefix = Invoice.get_invoices_with_prefix("INV")
+# for invoice in invoices_with_prefix:
+#     print(f"Invoice Number with prefix INV: {invoice.invoice_number}")
+#
+# # Get invoices sorted by invoice number
+# invoices_sorted = Invoice.get_invoices_sorted_by_number()
+# for invoice in invoices_sorted:
+#     print(f"Invoice Number: {invoice.invoice_number}")
+#
+# # Get an invoice by a specific invoice number along with its related billing info
+# invoice = Invoice.get_invoice_with_billing_info("INV002")
+# print(f"Invoice Number: {invoice.invoice_number}")
+# print(f"Billing Info: {invoice.billing_info.address}")
+
+
+
--- python_orm_oct_2023/advanced_queries_in_django/exercises/09-skeleton_exercises/main_app/models.py
+++ python_orm_oct_2023/advanced_queries_in_django/exercises/09-skeleton_exercises/main_app/models.py
@@ -1,7 +1,62 @@
+from decimal import Decimal
+
 from django.db import models
+from django.db.models import Count, Q, Max, Min, Avg, F
+from django.db.models.functions import Round
+
+from main_app.validators import video_game_rating, video_game_release_year
 
 
 # Create your models here.
+class RealEstateListingManager(models.Manager):
+    def by_property_type(self, property_type: str):
+        query = Q(property_type=property_type)
+        return RealEstateListing.objects.filter(query)
+
+    def in_price_range(self, min_price: Decimal, max_price: Decimal):
+        query = Q(price__gte=min_price) & Q(price__lte=max_price)
+        return RealEstateListing.objects.filter(query)
+
+    def with_bedrooms(self, bedrooms_count: int):
+        query = Q(bedrooms=bedrooms_count)
+        return RealEstateListing.objects.filter(query)
+
+    def popular_locations(self):
+        return (RealEstateListing.objects.values('location').
+                annotate(num_of_locations=Count('id')).order_by('id')[:2])
+
+
+class VideoGameManager(models.Manager):
+    def games_by_genre(self, genre: str):
+        query = Q(genre=genre)
+        return VideoGame.objects.filter(query)
+
+    def recently_released_games(self, year: int):
+        query = Q(release_year__gte=year)
+        return VideoGame.objects.filter(query)
+
+    def highest_rated_game(self):
+        highest_rating = VideoGame.objects.aggregate(highest_rating=Max('rating'))['highest_rating']
+        query = Q(rating=highest_rating)
+        game_instance = VideoGame.objects.get(query)
+
+        return game_instance
+
+    def lowest_rated_game(self):
+        lowest_rating = VideoGame.objects.aggregate(lowest_rating=Min('rating'))['lowest_rating']
+        query = Q(rating=lowest_rating)
+        game_instance = VideoGame.objects.get(query)
+
+        return game_instance
+
+    def average_rating(self):
+        avg_rating = VideoGame.objects.aggregate(avg_rating=Round(Avg('rating'), 1))['avg_rating']
+
+        return avg_rating
+
+        # avg_ratings = VideoGame.objects.annotate(avg_rating=Round(Avg('rating'), 1))
+        #
+        # return avg_ratings.order_by('-avg_rating')
 
 
 class RealEstateListing(models.Model):
@@ -18,6 +73,8 @@ class RealEstateListing(models.Model):
     bedrooms = models.PositiveIntegerField()
     location = models.CharField(max_length=100)
 
+    objects = RealEstateListingManager()
+
 
 class VideoGame(models.Model):
     GENRE_CHOICES = [
@@ -30,8 +87,19 @@ class VideoGame(models.Model):
 
     title = models.CharField(max_length=100)
     genre = models.CharField(max_length=100, choices=GENRE_CHOICES)
-    release_year = models.PositiveIntegerField()
-    rating = models.DecimalField(max_digits=2,decimal_places=1)
+    release_year = models.PositiveIntegerField(
+        validators=[video_game_release_year]
+    )
+    rating = models.DecimalField(
+        max_digits=2,
+        decimal_places=1,
+        validators=[video_game_rating]
+    )
+
+    objects = VideoGameManager()
+
+    def __str__(self):
+        return f"{self.title}"
 
 
 class BillingInfo(models.Model):
@@ -41,6 +109,24 @@ class BillingInfo(models.Model):
 class Invoice(models.Model):
     invoice_number = models.CharField(max_length=20, unique=True)
     billing_info = models.OneToOneField(BillingInfo, on_delete=models.CASCADE)
+
+    @staticmethod
+    def get_invoices_with_prefix(prefix):
+        query = Invoice.objects.filter(Q(invoice_number__startswith=prefix))
+
+        return query
+
+    @staticmethod
+    def get_invoices_sorted_by_number():
+        query = Invoice.objects.order_by('invoice_number')
+
+        return query
+
+    @staticmethod
+    def get_invoice_with_billing_info(invoice_number: str):
+        query = Invoice.objects.get(invoice_number=invoice_number)
+
+        return query
 
 
 class Technology(models.Model):
